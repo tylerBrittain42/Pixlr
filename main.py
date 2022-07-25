2a6e60607b4cbcd020c0f1eb215ae1be84241119
tbrittain2 <tbrittain2@ucmerced.edu>
2022-07-24 18:05:18 -0700

    able to print (not averaged) boxes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from audioop import avg
-from operator import le
+from itertools import count
+from operator import ge, le
 from tracemalloc import start
 from turtle import width
 from PIL import Image
@@ -9,6 +10,9 @@ import math
 
 
 def main():
+    width = 9
+    total_squares = width ** 2
+    
     checker = Image.open('./sample/checker.jpeg')
     # pix = checker.load()
     # foo = checker.split()
@@ -24,9 +28,62 @@ def main():
     avg_color = average_pix(checker,[0,0,1,1])
 
 
-    print(avg_color)
-    paint_box(checker,avg_color,[0,0,50,checker.width])
+    # print(avg_color)
+    # paint_box(checker,avg_color,[0,0,50,checker.width])
+    # checker.show()
+
+    
+    paint_boxes(checker, total_squares)
     checker.show()
+
+def paint_boxes(im,num):
+
+    row_num = int(math.sqrt(num))
+    col_num = int(math.sqrt(num))
+    total_box = num
+    
+    # Using cur_box to keep track of box being filled
+    # Assume we start at top right and work right then down
+    cur_x = 0
+    cur_y = 0
+
+    i = 255
+    j = 0
+    
+    box_width, box_height = get_box_size(im,row_num)
+
+    for row in range(row_num):
+        for col in range(col_num):
+            box = [cur_x,cur_y, cur_x + box_width, cur_y + box_height]
+            paint_box(im,[i,j,0],box)
+            if i == 255:
+                i = 0
+                j = 255
+            else:
+                i = 255
+                j = 0
+            print(f'row:{row} col:{col}\nx:{cur_x},y:{cur_y}\n')
+            cur_x += box_width
+        cur_y += box_height
+        cur_x = 0
+
+    # for row in range(row_num):
+    #     for col in range(col_num):
+    #         box = [cur_x,cur_y, cur_x + box_width, cur_y + box_height]
+    #         paint_box(im,[255,0,0],box)
+    #         print(f'x:{cur_x} y:{cur_y}')
+    #         cur_x += box_width
+    #     cur_x = 0
+    #     cur_y += box_height
+    # print(im.width)
+    print(box_width)
+
+
+    
+def get_box_size(im,box_num):
+    box_width = im.width // box_num
+    box_height = im.height // box_num
+    return [box_width, box_height]
 
 
 def paint_box(im, avg_color, box):
@@ -36,7 +93,7 @@ def paint_box(im, avg_color, box):
     # im = Image.merge((im.mode,source))
     # im.show()
 
-    start_x, start_y, height, width = box
+    start_x, start_y, width, height = box
     
 
     pix = im.load()
@@ -44,7 +101,7 @@ def paint_box(im, avg_color, box):
         for y in range(start_y,height):
             pix[x,y] = (avg_color[0], avg_color[1],avg_color[2])
             # print('a')
-    
+
 
 
 
