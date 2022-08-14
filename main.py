383f2e464b3021baf68b9f2a9b8e966a37ab2a85
tbrittain2 <tbrittain2@ucmerced.edu>
2022-08-14 16:15:51 -0400

    added demo, renamed method_2 to method_2

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from method_one import Pixlr
-
+from method_two import Pixlr
 
 
 def main():
+    print(f"Pixlr Demo")
+    file_name = input("File name:")
+    layer_choice = int(input("Layers:"))
     
-    method_one_demo = Pixlr('./sample/checker.jpeg',11)
-    method_one_demo.paint_boxes()
-    method_one_demo.show()
-   
-
-    
-    
-
-
+    method_two_demo = Pixlr(f"./sample/{file_name}.jpeg")
+    method_two_demo.multi_iteration(layer_choice)
+    method_two_demo.show()
 
 if __name__ == '__main__':
     main()
